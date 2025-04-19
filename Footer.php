3ccfc348273c69553512c5aded0738b902cfae82
Jahred Uy <khikho107@gmail.com>
2025-04-19 23:43:54 +0800

    Added dashboard design

---
+++ Footer.php
@@ -0,0 +1,3 @@
+<footer class="text-center" style="background-color: #f8f9fa; border-top: 1px solid #e9ecef; position: fixed; bottom: 0; left: 0; width: 100%; padding: 5px 0; font-family: 'Poppins', sans-serif; font-size: 0.9rem; color: #6c757d;">
+  <p class="mb-0">&copy; 2025 Alfonz Perez & Jahred Uy. All Rights Reserved.</p>
+</footer>
